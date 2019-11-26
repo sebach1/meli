713d221f41e2e2a53cc3200ee51cf2e0631f37f8
Sebastián Chamena <sebachamena@gmail.com>
2019-11-26 14:08:09 -0300

    Add creds entity

---
+++ creds.go
@@ -0,0 +1,29 @@
+package meli
+
+type token string
+
+type refreshToken token
+type accessToken token
+
+type creds struct {
+	Access        accessToken
+	Refresh       refreshToken
+	ApplicationId string
+	Secret        token
+}
+
+func (c *creds) validate() error {
+	if c.ApplicationId == "" {
+		return errNilApplicationId
+	}
+	if c.Secret == "" {
+		return errNilSecret
+	}
+	if c.Refresh == "" {
+		return errNilRefreshToken
+	}
+	if c.Access == "" {
+		return errNilAccessToken
+	}
+	return nil
+}
