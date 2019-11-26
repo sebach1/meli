100ceaad799142d1ff558e8486dd190db9d6af20
Sebastián Chamena <sebachamena@gmail.com>
2019-11-26 17:14:04 -0300

    Move product entity to its own file

--- meli.go
+++ meli.go
@@ -1,6 +1,8 @@
 package meli
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -60,4 +62,96 @@ func (ml *MeLi) Put(url string, body io.Reader) (resp *http.Response, err error)
 	}
 	req.Header.Set("Content-Type", "application/json")
 	return ml.Do(req)
+}
+
+func (ml *MeLi) GetProduct(id string) (*Product, error) {
+	URL, err := ml.RouteTo("product", id, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := ml.Get(URL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 400 {
+		return nil, errFromReader(resp.Body)
+	}
+	prod := &Product{}
+	err = json.NewDecoder(resp.Body).Decode(prod)
+	if err != nil {
+		return nil, err
+	}
+	return prod, nil
+}
+
+func (ml *MeLi) SetProduct(prod *Product) (newProd *Product, err error) {
+	if prod == nil {
+		return nil, errNilProduct
+	}
+	if prod.Id == "" {
+		newProd, err = ml.createProduct(prod)
+	} else {
+		newProd, err = ml.updateProduct(prod)
+	}
+	return
+}
+
+func (ml *MeLi) createProduct(prod *Product) (*Product, error) {
+	params, err := ml.paramsWithToken()
+	if err != nil {
+		return nil, err
+	}
+	URL, err := ml.RouteTo("product", "", params)
+	if err != nil {
+		return nil, err
+	}
+	jsonProd, err := json.Marshal(prod)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := ml.Post(URL, bytes.NewReader(jsonProd))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 400 {
+		return nil, errFromReader(resp.Body)
+	}
+	newProd := &Product{}
+	err = json.NewDecoder(resp.Body).Decode(newProd)
+	if err != nil {
+		return nil, err
+	}
+	return newProd, nil
+}
+
+func (ml *MeLi) updateProduct(prod *Product) (*Product, error) {
+	params, err := ml.paramsWithToken()
+	if err != nil {
+		return nil, err
+	}
+	URL, err := ml.RouteTo("product", prod.Id, params)
+	if err != nil {
+		return nil, err
+	}
+	prod.Id = "" // Unset id since its in the route
+	jsonProd, err := json.Marshal(prod)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := ml.Put(URL, bytes.NewReader(jsonProd))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 400 {
+		return nil, errFromReader(resp.Body)
+	}
+	newProd := &Product{}
+	err = json.NewDecoder(resp.Body).Decode(newProd)
+	if err != nil {
+		return nil, err
+	}
+	return newProd, nil
 }
--- meli_test.go
+++ meli_test.go
@@ -1,8 +1,11 @@
 package meli
 
 import (
+	"fmt"
 	"net/url"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestMeLi_RouteTo(t *testing.T) {
@@ -68,6 +71,149 @@ func TestMeLi_RouteTo(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("MeLi.RouteTo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMeLi_GetProduct(t *testing.T) {
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantProd *Product
+		wantErr  error
+		stub     *stub
+	}{
+		{
+			name:    "REMOTE returns an ERR",
+			wantErr: meliErrFooBar,
+			args:    args{id: "foo"},
+			stub: &stub{status: 404,
+				body: meliErrFooBar,
+			},
+		},
+		{
+			name:     "REMOTE returns the CORRECTly product",
+			wantErr:  nil,
+			wantProd: &Product{Id: "foo", Title: "bar"},
+			args:     args{id: "foo"},
+			stub: &stub{status: 200,
+				body: &Product{Id: "foo", Title: "bar"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ml := &MeLi{}
+			svClose := tt.stub.serve(t, ml)
+			defer svClose()
+
+			gotProd, err := ml.GetProduct(tt.args.id)
+
+			if fmt.Sprintf("%v", tt.wantErr) != fmt.Sprintf("%v", err) {
+				t.Errorf("MeLi.GetProduct() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if diff := cmp.Diff(tt.wantProd, gotProd); diff != "" {
+				t.Errorf("MeLi.GetProduct() mismatch (-want +got): %s", diff)
+			}
+		})
+	}
+}
+
+func TestMeLi_SetProduct(t *testing.T) {
+	tests := []struct {
+		name     string
+		creds    creds
+		prod     *Product
+		stub     *stub
+		wantProd *Product
+		wantErr  error
+	}{
+		{
+			name:    "but given NIL PRODUCT",
+			wantErr: errNilProduct,
+			creds:   creds{Access: "baz"},
+		},
+		{
+			name:    "but given NIL CREDENTIALS",
+			wantErr: errNilAccessToken,
+			prod:    &Product{Id: "foo", Title: "bar"},
+			creds:   creds{},
+		},
+		{
+			name:    "but given NIL CREDENTIALS",
+			wantErr: errNilAccessToken,
+			prod:    &Product{Id: "foo", Title: "bar"},
+			creds:   creds{},
+		},
+		{
+			name:     "while EDITing product REMOTE returns CORRECTly",
+			prod:     &Product{Id: "foo", Title: "bar"},
+			wantProd: &Product{Id: "foo", Title: "bar", Price: 0},
+			stub: &stub{status: 200,
+				wantBodyReceive: JSONMarshal(t, &Product{Title: "bar"}), // The body sent lacks of id since its in the route
+				wantParamsReceive: url.Values{
+					"access_token": []string{"baz"},
+				},
+				body: &Product{Id: "foo", Title: "bar", Price: 0},
+			},
+			creds: creds{Access: "baz"},
+		},
+		{
+			name:     "while CREATing product, REMOTE returns CORRECTly",
+			prod:     &Product{Title: "bar"},
+			wantProd: &Product{Id: "quux", Title: "bar"},
+			stub: &stub{status: 200,
+				wantBodyReceive: JSONMarshal(t, &Product{Title: "bar"}),
+				wantParamsReceive: url.Values{
+					"access_token": []string{"baz"},
+				},
+				body: &Product{Id: "quux", Title: "bar"},
+			},
+			creds: creds{Access: "baz"},
+		},
+		{
+			name:    "while CREATing product, REMOTE returns CORRECTly",
+			prod:    &Product{Title: "bar"},
+			wantErr: meliErrFooBar,
+			stub: &stub{status: 400,
+				wantBodyReceive: JSONMarshal(t, &Product{Title: "bar"}),
+				wantParamsReceive: url.Values{
+					"access_token": []string{"baz"},
+				},
+				body: meliErrFooBar,
+			},
+			creds: creds{Access: "baz"},
+		},
+		{
+			name:    "while EDITing product, REMOTE returns an ERROR",
+			prod:    &Product{Id: "foo", Title: "bar"},
+			wantErr: meliErrFooBar,
+			stub: &stub{status: 400,
+				wantBodyReceive: JSONMarshal(t, &Product{Title: "bar"}),
+				wantParamsReceive: url.Values{
+					"access_token": []string{"baz"},
+				},
+				body: meliErrFooBar,
+			},
+			creds: creds{Access: "baz"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ml := &MeLi{Credentials: tt.creds}
+			svClose := tt.stub.serve(t, ml)
+			defer svClose()
+
+			gotProd, err := ml.SetProduct(tt.prod)
+			if fmt.Sprintf("%v", tt.wantErr) != fmt.Sprintf("%v", err) {
+				t.Errorf("MeLi.SetProduct() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if diff := cmp.Diff(tt.wantProd, gotProd); diff != "" {
+				t.Errorf("MeLi.SetProduct() mismatch (-want +got): %s", diff)
 			}
 		})
 	}
