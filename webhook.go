00bfb9aec36974bec743ee726bf00e253cb9682a
Sebastián Chamena <sebachamena@gmail.com>
2019-11-29 10:54:17 -0300

    Add WebHook processors

--- webhook.go
+++ webhook.go
@@ -1,6 +1,9 @@
 package meli
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 type WebHook struct {
 	Resource      string `json:"resource,omitempty"`
@@ -11,4 +14,16 @@ type WebHook struct {
 
 	Sent     time.Time `json:"sent,omitempty"`
 	Received time.Time `json:"received,omitempty"`
+}
+
+func (ml *MeLi) ProcessProductWebHook(wh *WebHook) (*Product, error) {
+	return ml.GetProduct(ProductId(wh.ResourceID()))
+}
+
+func (wh *WebHook) ResourceID() string {
+	if wh.Resource == "" {
+		return ""
+	}
+	idx := strings.LastIndex(wh.Resource, "/")
+	return wh.Resource[0:idx]
 }
