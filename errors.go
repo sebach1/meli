0492d9af1a9b6865534ba7077be9812ca1d066b8
Sebastián Chamena <sebachamena@gmail.com>
2019-12-02 09:03:08 -0300

    Add not foudn errs

--- errors.go
+++ errors.go
@@ -13,9 +13,10 @@ var (
 	errNilRefreshToken  = errors.New("the REFRESH TOKEN is NIL")
 	errNilSecret        = errors.New("the SECRET is NIL")
 
-	errNilProductId = errors.New("the given PRODUCT ID is NIL")
-	errNilProduct   = errors.New("the given PRODUCT is NIL")
-	errNilVariant   = errors.New("the given VARIANT is NIL")
+	errNilProductId    = errors.New("the given PRODUCT ID is NIL")
+	errNilProduct      = errors.New("the given PRODUCT is NIL")
+	errNilVariant      = errors.New("the given VARIANT is NIL")
+	errVariantNotFound = errors.New("the given VARIANT does NOT EXISTS")
 	// errNilCategory     = errors.New("the given CATEGORY is NIL")
 	errNilCategoryId   = errors.New("the given CATEGORY ID is NIL")
 	errNilCombinations = errors.New("the given ATTR COMBINATIONS are NIL")
