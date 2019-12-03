f2f2b7567c3d8ca832a3cdccf3bcb141a2862022
Sebastián Chamena <sebachamena@gmail.com>
2019-12-03 16:43:20 -0300

    Remove picId due its treated as src in remote

--- picture.go
+++ picture.go
@@ -1,13 +1,11 @@
 package meli
 
-type PictureId string
-
 type Picture struct {
-	Id        PictureId `json:"id,omitempty"`
-	Source    string    `json:"source,omitempty"`
-	URL       string    `json:"url,omitempty"`
-	SecureURL string    `json:"secure_url,omitempty"`
-	Size      string    `json:"size,omitempty"`
-	MaxSize   string    `json:"max_size,omitempty"`
-	Quality   string    `json:"quality,omitempty"`
+	Id        string `json:"id,omitempty"`
+	Source    string `json:"source,omitempty"`
+	URL       string `json:"url,omitempty"`
+	SecureURL string `json:"secure_url,omitempty"`
+	Size      string `json:"size,omitempty"`
+	MaxSize   string `json:"max_size,omitempty"`
+	Quality   string `json:"quality,omitempty"`
 }
