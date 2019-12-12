08cd13c7c865186b74935069746801f61afda633
Sebastián Chamena <sebachamena@gmail.com>
2019-12-11 22:27:08 -0300

    Improve comment of assist pkg

--- internal/test/assist/assist.go
+++ internal/test/assist/assist.go
@@ -1,6 +1,8 @@
 /*
-Package assist contains testing helpers but for initialization.
+Package assist contains testing helpers but for init()
+-not for testing init func, but for using init on testing-
 
-Because of that, that type of funcs cannot contain *t testing.T arg on any function, so the errs are notified by log.Fatal (due every bad initialization causes bad processing).
+Because of that, that type of funcs couldnt contain testing.T arg on any function,
+and then the errs are notified by log.Fatal (due every bad init would causes bad processing).
 */
 package assist
